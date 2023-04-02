356fd47bbf79976d6a47aa3d1fdfa8eab6635c3f
Ankit0225 <ankitmaurya0225@gmail.com>
2023-04-02 18:09:21 +0530

    added some  effect test it will it add more if approved

--- frontend/price-wise-vite/src/components/DashBoard.jsx
+++ frontend/price-wise-vite/src/components/DashBoard.jsx
@@ -1,7 +1,18 @@
 import React from "react";
+import { useDisclosure } from "@mantine/hooks";
+import {Drawer} from '@mantine/core';
+
+
 function DashBoard() {
+const [opened, {open,close}]= useDisclosure(false);
   return (
+    <>
+     
+     <Drawer opened={opened} onClose={close} title="Focus demo" position="bottom" size="50%" overlayProps={{ opacity: 0.5, blur: 4 }}>
+              <p className='text-center text-2xl text-bold'>Make Payment</p>
+            </Drawer>
     <div className="flex flex-col  p-4 bg-indigo-700 w-screen rounded-xl mt-4 m-2">
+      
       <div className="text-2xl font-semibold "></div>
       <div className="input flex justify-between">
         <input type="text" />
@@ -11,12 +22,17 @@ function DashBoard() {
       </div>
       <div className="multi-container grid text-white ">
         <div className="text mt-5">Our services</div>
-        <div className="grid  grid-cols-6 gap-4 mt-5 place-content-center text-center text-white w-full h-60">
+        <div className="grid sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-6 gap-4 mt-5 place-content-center text-center text-white w-full h-auto">
           <div className="box-1 ">
             <div className="icon"></div>
             Digital Banking
           </div>
-          <div className="box-1 ">Expense Analysis</div>
+          <div className="box-1 bg-red-100 h-60 w-full" onClick={open} onClose={close}>
+           <p>
+            Expense Analysis
+            </p> 
+            
+          </div>
           <div className="box-1 ">Budget Planning</div>
           <div className="box-1 ">Scheduled Payments</div>
           <div className="box-1 ">Auto Pay</div>
@@ -25,7 +41,7 @@ function DashBoard() {
       </div>
       <div className="down-multi-container text-white">
         <div className="down-text my-6">Today's Insights</div>
-        <div className="down-container grid grid-cols-3 gap-3 text-center place-content-center h-60 w-full">
+        <div className="down-container grid sm:grid-cols-1 md:grid-cols-3 lg:grid-cols-3 gap-3 text-center place-content-center h-60 w-full">
           <div className="down-first-container">
             <div className="down-first-text">Requests</div>
           </div>
@@ -38,6 +54,7 @@ function DashBoard() {
         </div>
       </div>
     </div>
+    </>
   );
 }
 
